c541071896b52d073948cff7d6689344070c88f1
nikhilrock1212 <142542615+nikhilrock1212@users.noreply.github.com>
2024-06-01 23:32:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['Web Developer ','Frontend developer','Software Developer'],
+    strings:['Graduate Engineer','Web Developer ','Frontend developer',],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
